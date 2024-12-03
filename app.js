569f0c59674a1d45aab5c1836b17c24140cf363c
davidalemmons <davidalemmons@gmail.com>
2024-12-03 17:38:09 -0500

    Final tutorial fore node in web development class

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
@@ -12,8 +12,6 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(err => console.log(err));
 
 app.set('view engine', 'ejs');
-
-// middleware & static files
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
@@ -22,7 +20,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// routes
 app.get('/', (req, res) => {
     res.redirect('/blogs');
 });
@@ -31,55 +28,7 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
-// blog routes
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-});
-
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-        res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-    .then(result => {
-        res.redirect('/blogs');
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-    .then(result => {
-        res.render('details', { blog: result, title: 'Blog Details' });
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-    .then(result => {
-        res.json({ redirect: '/blogs' });
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
+app.use('/blogs', blogRoutes);
 
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
